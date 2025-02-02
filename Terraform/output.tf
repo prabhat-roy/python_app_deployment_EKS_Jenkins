b45b119859661b3e73f2d7580091e206bd7c71e9
Prabhat <prabhatkumar.roy@outlook.com>
2025-02-03 04:43:18 +0530

    first commit

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,3 +1,6 @@
 output "jenkins" {
   value = join("", ["http://", aws_instance.jenkins.public_ip, ":", "8080"])
 }
+output "SonarQube" {
+  value = join("", ["http://", aws_instance.jenkins.public_ip, ":", "9000"])
+}
