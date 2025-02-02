cbcb7f9b99e45e86526519d2dfd96366c549e9e3
Prabhat <prabhatkumar.roy@outlook.com>
2025-02-03 02:04:36 +0530

    first commit

--- Terraform/EKS/iam.tf
+++ Terraform/EKS/iam.tf
@@ -32,53 +32,6 @@ resource "aws_iam_policy" "AmazonEKSAssumeEKSAdminPolicy" {
   policy = file("AmazonEKSAssumeEKSAdminPolicy.json")
 }
 
-resource "aws_iam_policy" "ecrimage" {
-  name   = "ECR_image_upload_policy"
-  policy = file("ecr_image_upload.json")
-}
-
-resource "aws_iam_user" "jenkins-eks" {
-  name = "eks-jenkins"
-}
-
-resource "aws_iam_user_policy_attachment" "eks-jenkins-policy" {
-  user       = aws_iam_user.jenkins-eks.name
-  policy_arn = aws_iam_policy.AmazonEKSAssumeEKSAdminPolicy.arn
-}
-
-resource "aws_iam_user_policy_attachment" "ecr-jenkins-policy" {
-  user       = aws_iam_user.jenkins-eks.name
-  policy_arn = aws_iam_policy.ecrimage.arn
-}
-
-resource "aws_iam_role" "jenkins_admin_role" {
-  name               = "jenkins_admin_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "jenkins_attachment" {
-  role       = aws_iam_role.jenkins_admin_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-resource "aws_iam_instance_profile" "jenkins_admin_profile" {
-  name = "jenkins_admin_profile"
-  role = aws_iam_role.jenkins_admin_role.name
-}
-
 resource "aws_iam_role" "eks-iam-role" {
   name = "eks-iam-role"
 
